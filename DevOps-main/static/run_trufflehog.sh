d4a7b2c1e408563eb5f8a853211c6cc64e90f5c1
Бахтовар Пардаев <113818209+anitibia@users.noreply.github.com>
2024-12-30 17:53:48 +0300

    Update run_trufflehog.sh

--- DevOps-main/static/run_trufflehog.sh
+++ DevOps-main/static/run_trufflehog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-trufflehog filesystem --directory ./ > ./trufflehog_report.txt
+trufflehog filesystem --directory ./ > trufflehog_report.txt
 if grep -q "Secrets Found" trufflehog_report.txt; then
     echo "TruffleHog обнаружил секреты в коде!"
-    cat ./trufflehog_report.txt
+    cat trufflehog_report.txt
     exit 1
 fi
 echo "No critical issues found."
