a151c95582ac3f8b5c5de84f3c1c1a6e1ff5fd6a
Бахтовар Пардаев <113818209+anitibia@users.noreply.github.com>
2024-12-30 16:20:09 +0300

    Create run_trufflehog.sh

---
+++ DevOps-main/static/run_trufflehog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+trufflehog filesystem --directory ./ > ./trufflehog_report.txt
+if grep -q "Secrets Found" trufflehog_report.txt; then
+    echo "TruffleHog обнаружил секреты в коде!"
+    cat ./trufflehog_report.txt
+    exit 1
+fi
