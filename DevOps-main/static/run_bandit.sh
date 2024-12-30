cb159c092f71d568352877f619ad7ebb34f56a36
Бахтовар Пардаев <113818209+anitibia@users.noreply.github.com>
2024-12-30 17:50:58 +0300

    Update run_bandit.sh

--- DevOps-main/static/run_bandit.sh
+++ DevOps-main/static/run_bandit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-bandit -r . --severity-level high --exit-zero > ./bandit_report.txt
-if grep -q "HIGH" bandit_report.txt; then
-    echo "Bandit обнаружил уязвимости высокой критичности!"
-    cat ./bandit_report.txt
+bandit -r ./project_directory -ll -f txt -o bandit_report.txt
+
+if grep -q 'CRITICAL' bandit_report.txt; then
+    echo "Critical issues found in the code!"
     exit 1
 fi
+echo "No critical issues found."
+exit 0
+
+
+
