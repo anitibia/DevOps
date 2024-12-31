87b7312ae70e6cc5688b645b48b626d3f3d45ece
Бахтовар Пардаев <113818209+anitibia@users.noreply.github.com>
2024-12-31 06:22:54 +0300

    Update app.py

--- DevOps-main/app.py
+++ DevOps-main/app.py
@@ -10,7 +10,6 @@ import time
 from prometheus_client import Counter, generate_latest, REGISTRY
 from faker import Faker
 
-#s#@#d
 
 try:
     engine = create_engine('postgresql+psycopg2://postgres:909909@postgres-db:5432/devOps-db')
